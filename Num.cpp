0cc0feaa04c9566ca4fc59e0498efc28faa29939
OchenYstal <ystal242@gmail.com>
2025-07-16 15:43:16 +0300

    Просты числа

--- Num.cpp
+++ Num.cpp
@@ -1,3 +1,40 @@
-//
-// Created by ystal on 6/22/25.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int n) {// вычисление простых чисел
+    if (n <= 1) return false;
+    for (int i = 2; i <= sqrt(n); ++i)
+        if (n % i == 0) {
+            return false;
+        }
+    return true;
+}
+void printPrime(int lim) {
+    int index = 0;
+    vector<int> primes;
+
+    cout << "prostie chisla ot 1 do " << lim << endl;
+    for (int i = 2; i <= lim; ++i, ++index) {
+        if (isPrime(i)) {
+            primes.push_back(i);
+        }
+    }
+    for (int x : primes) {
+        cout << x << endl;
+    }
+    cout << endl;
+}
+int main() {
+    printPrime(100);
+    int max;
+    cout << "Vvedite max num dlya poiska prostyh chisel: ";
+    cin >> max;
+
+    printPrime(max);
+
+    return 0;
+}
