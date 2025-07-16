30994f1851f3141335a1df5247d7edd9834bfd89
OchenYstal <ystal242@gmail.com>
2025-07-16 15:44:44 +0300

    Решение квадратичных уравнений

--- vigaril.cpp
+++ vigaril.cpp
@@ -1,3 +1,24 @@
-//
-// Created by ystal on 6/27/25.
-//
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() { //Программа для решение квадратичного уравнение
+
+   double a, b, c;
+      cout << "Введите 3 значения: " << endl;
+      cin >> a >> b >> c;
+      if (a == 0) {
+         cout << "neza tak delat!!!" << endl;
+      }
+      cout << a << "x^2 + " << b << "x + " << c << " = 0" << endl;
+      double Res = sqrt((b * b)) + 4 * a * c; //Возводит в квадрат
+      cout << "D = " << b << "^2 + " << "4 * " << a << " * " << c << " = " << Res << endl;
+      double squareR = sqrt(Res);
+      cout << "x1 = " << "-b + √D / 2a = " << b << " + " << squareR << " / 2 * " << a << " = " << b + squareR / 2 * a << endl;
+      cout << "x2 = " << "-b - √D / 2a = " << b << " + " << squareR << " / 2 * " << a << " = " << b - squareR / 2 * a << endl;
+
+   return 0;
+}
